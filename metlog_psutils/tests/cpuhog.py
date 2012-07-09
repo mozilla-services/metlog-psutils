d1c15403328369a44a1456069a03428e46e45fed
Victor Ng <victor@crankycoder.com>
2012-07-09 13:13:47 -0400

    updated the cpuhog script for test_psutils

--- metlog_psutils/tests/cpuhog.py
+++ metlog_psutils/tests/cpuhog.py
@@ -2,13 +2,13 @@
 This is used by metlog-psutils's test to check for out of process CPU
 usage
 """
-import time
+
+
 def main():
-    time.sleep(1)
-    for i in range(20000):
+    for i in range(200000):
         for j in range(7, 20):
             x = j ** 25
-    time.sleep(1)
+    x = x
 
 if __name__ == '__main__':
     main()
