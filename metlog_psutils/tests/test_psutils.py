2243fe52b83125eda4f3f1ab90312ebd9e4059b9
Victor Ng <victor@crankycoder.com>
2012-07-09 12:25:10 -0400

    reworked the testcases to use the DebugSender instead of one of the underlying plugin methods

--- metlog_psutils/tests/test_psutils.py
+++ metlog_psutils/tests/test_psutils.py
@@ -16,38 +16,43 @@ We are initially interested in :
     * memory utilization.
 """
 
-from metlog_psutils.psutil_plugin import check_osx_perm
 import sys
-from metlog_psutils.psutil_plugin import process_details
 from metlog_psutils.psutil_plugin import supports_iocounters
-from mock import Mock
 from nose.tools import eq_
 from metlog.config import client_from_text_config
 import json
-
 from unittest2 import TestCase
 import itertools
-import socket
-import threading
 import time
 import subprocess
 
 
-class TestProcessLogs(TestCase):
+class TestPsutil(TestCase):
+    def setUp(self):
+        cfg_txt = """
+        [metlog]
+        sender_class = metlog.senders.DebugCaptureSender
+
+        [metlog_plugin_psutil]
+        provider=metlog_psutils.psutil_plugin:config_plugin
+        """
+        self.client = client_from_text_config(cfg_txt, 'metlog')
 
     def test_cpu_info(self):
-        detail = process_details(cpu=True)
+        self.client.procinfo(cpu=True)
+        detail = list(self.client.sender.msgs)
 
         found_pcnt = False
         found_sys = False
         found_user = False
-        for statsd in detail['cpu']:
-            if statsd['key'] == 'pcnt':
-                found_pcnt = True
-            if statsd['key'] == 'sys':
-                found_sys = True
-            if statsd['key'] == 'user':
-                found_user = True
+
+        for statsd in [json.loads(msg) for msg in detail]:
+            if statsd['fields']['name'] == 'pcnt':
+                found_pcnt = statsd['payload']
+            if statsd['fields']['name'] == 'sys':
+                found_sys = statsd['payload']
+            if statsd['fields']['name'] == 'user':
+                found_user = statsd['payload']
         assert found_pcnt and found_sys and found_user
 
     def test_busy_info(self):
@@ -57,132 +62,86 @@ class TestProcessLogs(TestCase):
         proc = subprocess.Popen([sys.executable, '-m',
             'metlog_psutils.tests.cpuhog'])
         pid = proc.pid
-        time.sleep(1)
-        detail = process_details(pid=pid, busy=True)
+        time.sleep(0.5)
+        self.client.procinfo(pid=pid, busy=True)
         proc.communicate()
 
-        assert len(detail['busy']) == 3
-        for statsd in detail['busy']:
-            if statsd['key'] == 'total_cpu':
-                found_total = True
-            if statsd['key'] == 'uptime':
-                found_uptime = True
-            if statsd['key'] == 'pcnt':
-                found_pcnt = True
+        detail = list(self.client.sender.msgs)
+
+        assert len(detail) == 3
+        for statsd in [json.loads(msg) for msg in detail]:
+            if statsd['fields']['name'] == 'total_cpu':
+                found_total = statsd['payload']
+            if statsd['fields']['name'] == 'uptime':
+                found_uptime = statsd['payload']
+            if statsd['fields']['name'] == 'pcnt':
+                found_pcnt = statsd['payload']
 
         assert found_total and found_uptime and found_pcnt
 
     def test_thread_cpu_info(self):
-        detail = process_details(threads=True)
+        self.client.procinfo(threads=True)
+        detail = list(self.client.sender.msgs)
 
-        msgs = detail['threads']
+        msgs = [json.loads(msg) for msg in detail]
 
+        # Make sure that we have some thread info
         assert len(msgs) > 0
-        for k, g in itertools.groupby(msgs,\
-                lambda x: x['ns'] + '.' + x['key'].split(".")[0]):
+
+        # Group the msgs by thread ID number
+        thread_groups = itertools.groupby(msgs, \
+                lambda x: x['fields']['name'].split(".")[0])
+
+        for k, g in thread_groups:
             g_list = list(g)
+
+            # Each thread should have sys and user information
             eq_(len(g_list), 2)
-            eq_(1, len([f['key'] for f in g_list if
-                f['key'].endswith('.sys')]))
-            eq_(1, len([f['key'] for f in g_list if
-                f['key'].endswith('.user')]))
+            eq_(1, len([f for f in g_list if
+                f['fields']['name'].endswith('.sys')]))
+            eq_(1, len([f for f in g_list if
+                f['fields']['name'].endswith('.user')]))
 
     def test_io_counters(self):
         if not supports_iocounters():
             self.skipTest("No IO counter support on this platform")
 
-        detail = process_details(io=True)
+        self.client.procinfo(io=True)
+        detail = [json.loads(msg) for msg in self.client.sender.msgs]
 
         found_rb = False
         found_wb = False
         found_rc = False
         found_wc = False
         for statsd in detail['io']:
-            if statsd['key'] == 'read_bytes':
-                found_rb = True
-            if statsd['key'] == 'write_bytes':
-                found_wb = True
-            if statsd['key'] == 'read_count':
-                found_rc = True
-            if statsd['key'] == 'write_count':
-                found_wc = True
+            if statsd['fields']['name'] == 'read_bytes':
+                found_rb = statsd['payload']
+            if statsd['fields']['name'] == 'write_bytes':
+                found_wb = statsd['payload']
+            if statsd['fields']['name'] == 'read_count':
+                found_rc = statsd['payload']
+            if statsd['fields']['name'] == 'write_count':
+                found_wc = statsd['payload']
         assert found_wc and found_rc and found_wb and found_rb
 
     def test_meminfo(self):
         found_pcnt = False
         found_rss = False
         found_vms = False
-        detail = process_details(mem=True)
-        for statsd in detail['mem']:
-            eq_(len(statsd), 4)
-            assert statsd['ns'].startswith('psutil.meminfo')
-            if statsd['key'] == 'pcnt':
-                found_pcnt = True
-            if statsd['key'] == 'rss':
-                found_rss = True
-            if statsd['key'] == 'vms':
+
+        self.client.procinfo(mem=True)
+        detail = [json.loads(msg) for msg in self.client.sender.msgs]
+
+        for statsd in detail:
+            assert statsd['fields']['logger'].startswith('psutil.meminfo')
+            if statsd['fields']['name'] == 'pcnt':
+                found_pcnt = statsd['payload']
+            if statsd['fields']['name'] == 'rss':
+                found_rss = statsd['payload']
+            if statsd['fields']['name'] == 'vms':
                 found_vms = True
 
         assert found_pcnt and found_rss and found_vms
-
-
-class TestMetlog(object):
-    logger = 'tests'
-
-    def setUp(self):
-
-        cfg_txt = """
-        [metlog]
-        sender_class = metlog.senders.DebugCaptureSender
-
-        [metlog_plugin_procinfo]
-        provider=metlog_psutils.psutil_plugin:config_plugin
-        net=True
-        """
-        ###
-        self.client = client_from_text_config(cfg_txt, 'metlog')
-
-    def test_add_procinfo(self):
-        HOST = 'localhost'         # Symbolic name meaning the local host
-        PORT = 50017               # Arbitrary non-privileged port
-
-        def echo_serv():
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-            s.bind((HOST, PORT))
-            s.listen(1)
-
-            conn, addr = s.accept()
-            data = conn.recv(1024)
-            conn.send(data)
-            conn.close()
-            s.close()
-
-        t = threading.Thread(target=echo_serv)
-        t.start()
-        time.sleep(1)
-
-        def client_code():
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((HOST, PORT))
-            client.send('Hello, world')
-            client.recv(1024)
-            client.close()
-            time.sleep(1)
-
-        self.client.procinfo(net=True, server_addr='localhost:50017')
-
-        msgs = list(self.client.sender.msgs)
-        eq_(len(msgs), 1)
-        msg = json.loads(msgs[0])
-
-        assert msg['fields']['logger'].startswith("psutil.net")
-        assert msg['fields']['name'] == '127_0_0_1:50017.LISTEN'
-
-        # Start the client up just so that the server will die gracefully
-        tc = threading.Thread(target=client_code)
-        tc.start()
 
 
 class TestConfiguration(object):
@@ -193,8 +152,6 @@ class TestConfiguration(object):
     logger = 'tests'
 
     def setUp(self):
-        self.mock_sender = Mock()
-
         cfg_txt = """
         [metlog]
         sender_class = metlog.senders.DebugCaptureSender
@@ -216,7 +173,6 @@ def test_plugins_config():
 
     [metlog_plugin_procinfo]
     provider=metlog_psutils.psutil_plugin:config_plugin
-    cpu=True
     """
 
     client = client_from_text_config(cfg_txt, 'metlog')
